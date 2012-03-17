b255d40ce84a482edd3f331dd554e0ea0ce1946b
Jake Drahos <jake@laptop.(none)>
2012-03-16 23:09:27 -0500

    Began modifications to allow for standings tracking

---
+++ parse-standings.sh
@@ -0,0 +1,56 @@
+#! /bin/bash
+#####Begin Raw Data Acquisition#####
+unzip $FILENAME
+
+rm -f $FILENAME
+
+cp -r Documents/1/Pages ./
+
+rm -r Pages/_rels
+
+pushd Pages
+
+cat `ls` | grep -i unicodestring= | cut -d '"' -f 16 | tail -n +6 | head -n -1 > ../../output/standings
+cat `ls` | grep -i unicodestring= | cut -d '"' -f 16 | head -n 1 > ../../output/event
+
+popd
+
+#####CLEANUP TARGET#####
+
+for FILE in `ls`
+do
+	rm -r $FILE 
+done
+popd
+
+#####Begin Formatting Pairings as Tab-Separated Values#####
+
+pushd output
+
+echo -n "" > standings.tsv
+
+cat pairings | while read LINE_CONTENTS
+do
+for ELEMENT in $LINE_CONTENTS
+do
+	echo -n $ELEMENT >> standings.tsv
+	echo -n '	' >> standings.tsv
+done
+echo >> standings.tsv
+done
+rm standings
+mv standings.tsv standings
+popd
+
+#####Send data to server and call MySQL Refresh Script#####
+
+ssh $SERVER_USERNAME@$SERVER_ADDRESS -p $SERVER_PORT 'rm -r tournament-server/output'
+
+scp -r  -P $SERVER_PORT output $SERVER_USERNAME@$SERVER_ADDRESS:tournament-server/
+
+ssh $SERVER_USERNAME@$SERVER_ADDRESS -p $SERVER_PORT 'tournament-server/refresh-mysql-standings.sh'
+
+#####Call MySQL Refresh Script Locally#####
+rm -r ~/tournament-server/output
+cp -r output ~/tournament-server/output
+~/tournament-server/refresh-mysql-local-standings.sh
--- parser.sh
+++ parser.sh
@@ -41,62 +41,11 @@ do
 done
 echo ""	
 echo "Write Finished. Parsing file"
-#####Begin Raw Data Acquisition#####
 
-unzip $FILENAME
-
-rm -f $FILENAME
-
-cp -r Documents/1/Pages ./
-
-rm -r Pages/_rels
-
-pushd Pages
-
-cat `ls` | grep -i unicodestring= | cut -d '"' -f 16 | tail -n +6 | head -n -1 > ../../output/pairings
-cat `ls` | grep -i unicodestring= | cut -d '"' -f 16 | head -n 1 > ../../output/event
-cat `ls` | grep -i "unicodestring=\"round" | cut -d '"' -f 16 | cut -d ' ' -f 2 > ../../output/round
-
-popd
-
-#####CLEANUP TARGET#####
-
-for FILE in `ls`
-do
-	rm -r $FILE 
-done
-popd
-
-#####Begin Formatting Pairings as Tab-Separated Values#####
-
-pushd output
-
-echo -n "" > pairings.tsv
-
-cat pairings | while read LINE_CONTENTS
-do
-for ELEMENT in $LINE_CONTENTS
-do
-	echo -n $ELEMENT >> pairings.tsv
-	echo -n '	' >> pairings.tsv
-done
-echo >> pairings.tsv
-done
-rm pairings
-mv pairings.tsv pairings
-popd
-
-#####Send data to server and call MySQL Refresh Script#####
-
-ssh $SERVER_USERNAME@$SERVER_ADDRESS -p $SERVER_PORT 'rm -r tournament-server/output'
-
-scp -r  -P $SERVER_PORT output $SERVER_USERNAME@$SERVER_ADDRESS:tournament-server/
-
-ssh $SERVER_USERNAME@$SERVER_ADDRESS -p $SERVER_PORT 'tournament-server/refresh-mysql.sh'
-
-#####Call MySQL Refresh Script Locally#####
-rm -r ~/tournament-server/output
-cp -r output ~/tournament-server/output
-~/tournament-server/refresh-mysql-local.sh
-
+if [ `cat $FILENAME | grep -i pairings` == "" ]
+then
+	source ../parse-standings.sh
+else
+	source ../parse-pairings.sh
+fi
 done
